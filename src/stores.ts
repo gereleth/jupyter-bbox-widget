fb12eda20d07a3122e87daec1b426ebeaa01c9e4
Daria Vasyukova <daria.voznyak@gmail.com>
2021-05-25 20:05:38 +0300

    Take initial value from python

--- src/stores.ts
+++ src/stores.ts
@@ -3,8 +3,11 @@ import { writable } from 'svelte/store';
 
 export function createValue(model: any, name_: string, value_: any) {
   const name: string = name_;
-  const curVal: Writable<any> = writable(value_);
-
+  // use value from python if it is set when creating widget
+  const modelValue = model.get(name);
+  const curVal: Writable<any> = writable(
+    modelValue === undefined ? value_ : modelValue
+  );
   model.on('change:' + name, () => curVal.set(model.get(name)), null);
 
   return {
