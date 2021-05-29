fb38a1c5d814998693f50791658abf5e47613ea9
Daria Vasyukova <daria.voznyak@gmail.com>
2021-05-29 17:50:03 +0300

    Add default values to mock model

--- src/mock.ts
+++ src/mock.ts
@@ -6,6 +6,14 @@ class MockModel {
   undo() {}
   on(variableName: string, callback: () => void, context: any) {}
   save_changes() {}
+  get(name: string) {
+    if (name === 'image') {
+      return 'https://images.unsplash.com/photo-1612200538165-26e793ae757f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1867&q=80';
+    } else if (name === 'classes') {
+      return ['apple', 'orange', 'pear'];
+    }
+    return undefined;
+  }
 }
 
 const app = new BBoxWidget({
